06344be3d28ac1cebac42104757a73a4b8209533
sabznathan <sabarinathantms@gmail.com>
2022-02-13 09:54:07 +0530

    Initial Commit

---
+++ src/org/looginh/ControlStatementLooping.java
@@ -0,0 +1,17 @@
+package org.looginh;
+
+
+public class ControlStatementLooping {
+
+public static void main(String[] args) {
+
+int a=0;
+do {
+	System.out.println("print"+a);
+	a++;
+	System.exit(a);
+} while (a<=10);
+	}
+
+	
+}
